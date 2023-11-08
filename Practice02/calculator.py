b347d1b7f186e992eb3d8640662af3dd67b626fd
karmishthagitactions <karmishtha03@gmail.com>
2023-11-08 11:26:34 +0530

    Prg01

---
+++ Practice02/calculator.py
@@ -0,0 +1,21 @@
+"""
+Calculator Module
+
+This module provides functions for basic arithmetic calculations.
+"""
+
+# Constants (following UPPER_CASE naming style)
+A = 1
+B = 2
+
+# Perform calculations
+SUM = A + B
+DIFFERENCE = A - B
+QUOTIENT = A / B
+PRODUCT = A * B
+
+# Output results
+print("Sum:", SUM)
+print("Difference:", DIFFERENCE)
+print("Quotient:", QUOTIENT)
+print("Product:", PRODUCT)
